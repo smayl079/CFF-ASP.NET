9da464a58852d3073ce7e73f5466e6ea8653c042
İsmayıl Qafarlı <154833217+smayl079@users.noreply.github.com>
2025-12-08 20:34:19 +0400

    Add files via upload

---
+++ Task1/FileName.cs
@@ -0,0 +1,58 @@
+﻿
+
+
+// File: Controllers/ContactController.cs
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace MyWebApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            // This will render Views/Contact/Index.cshtml
+            return View();
+        }
+    }
+}
+
+
+// File: Controllers/ProductsController.cs
+
+
+
+namespace MyWebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            // This will render Views/Products/Index.cshtml
+            return View();
+        }
+
+
+        // Example of another action
+        public IActionResult Details(int id)
+        {
+            ViewBag.ProductId = id;
+            return View(); // Views/Products/Details.cshtml
+        }
+    }
+}
+
+
+
+
+
+namespace MyWebApp.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // Views/Services/Index.cshtml
+        }
+    }
+}
